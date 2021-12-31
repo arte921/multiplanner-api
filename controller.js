3b0238f6ef02a791d5c15308665404a1b7b3e31d
Ard-Jan van Etten <ardjanve@gmail.com>
2021-12-31 10:49:45 +0100

    fix ze controller

--- controller.js
+++ controller.js
@@ -21,14 +21,15 @@ const leesMap = require("./functies/leesMap.js");
             .on('error', console.error)
             .on('data', e => body.push(e))
             .on('end', async () => {
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-                res.setHeader('Access-Control-Allow-Headers', '*');
+                response.setHeader('Access-Control-Allow-Origin', '*');
+                response.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                response.setHeader('Access-Control-Allow-Headers', '*');
 
                 body = Buffer.concat(body).toString();
                 response.on('error', console.error);
             
                 const service = services[url.slice(1)];
+                console.log(body);
 
                 service
                     .service(body)
