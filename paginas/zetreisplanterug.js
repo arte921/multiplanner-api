b0b1fd2d3aa187b3e16c1088ada7e387f1792ad5
Ard-Jan van Etten <ardjanve@gmail.com>
2022-01-01 14:16:54 +0100

    verberg tekstverwijzing

--- paginas/zetreisplanterug.js
+++ paginas/zetreisplanterug.js
@@ -2,4 +2,4 @@ if (document.cookie) document.getElementById("textarea").value = decodeURICompon
 
 axios
     .get("tekst?filter=%5E(Proverbs%7CEcclesiastes)%20%5B0-9%5D%2B%20%5B0-9%2B%5D")
-    .then((resultaat) => document.getElementById("bijbeltekst").innerHTML = resultaat.data);
+    .then((resultaat) => document.getElementById("bijbeltekst").innerHTML = resultaat.data.match(/(?<=^[0-9A-Za-z ]+ )[^0-9]+$/)[0]);
