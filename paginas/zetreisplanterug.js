92b28b8e81722d96d7b22721693f74fcdbdbcfd3
Ard-Jan van Etten <ardjanve@gmail.com>
2022-01-01 14:11:58 +0100

    tekst homepagina

--- paginas/zetreisplanterug.js
+++ paginas/zetreisplanterug.js
@@ -1,5 +1,5 @@
 if (document.cookie) document.getElementById("textarea").value = decodeURIComponent(document.cookie);
 
 axios
-    .get("http://arte921.duckdns.org/tekst?filter=%5E(Proverbs%7CEcclesiastes)%20%5B0-9%5D%2B%20%5B0-9%2B%5D", reisplan)
-    .then((resultaat) => document.getElementById("bijbeltekst").value = resultaat);
+    .get("tekst?filter=%5E(Proverbs%7CEcclesiastes)%20%5B0-9%5D%2B%20%5B0-9%2B%5D")
+    .then((resultaat) => document.getElementById("bijbeltekst").innerHTML = resultaat.data);
