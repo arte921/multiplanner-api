7fac50f17bf453371960f00c999bb22c0de047b8
Ard-Jan van Etten <ardjanve@gmail.com>
2022-01-10 13:59:57 +0100

    e

--- paginas/zetreisplanterug.js
+++ paginas/zetreisplanterug.js
@@ -1,7 +1,7 @@
 
 axios
-.get("tekst?filter=(ecclesiastes%7Cproverbs%7Cmattheus%7Cmark%7Cluke%7Cjohn)")
-.then((resultaat) => document.getElementById("bijbeltekst").innerHTML = resultaat.data.match(/(?<=^[0-9A-Za-z ]+ )[^0-9]+$/)[0]);
+    .get("tekst?filter=(ecclesiastes%7Cproverbs%7Cmattheus%7Cmark%7Cluke%7Cjohn)")
+    .then((resultaat) => document.getElementById("bijbeltekst").innerHTML = resultaat.data.match(/(?<=^[0-9A-Za-z ]+ )[^0-9]+$/)[0]);
 
 if (document.cookie) {
     document.getElementById("textarea").value = decodeURIComponent(document.cookie);
